fa6480f7a2cd3d3135a0b6656a959b5639007d78
IT-Kitty <katerina.kvtn.skh@gmail.com>
2025-12-14 08:26:09 +0300

    feat: Implement flat JSON diff

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-import parseFile from './src/parser.js';
+import _ from 'lodash'
+import parseFile from './src/parser.js'
 
 const genDiff = (filepath1, filepath2) => {
-  const data1 = parseFile(filepath1);
-  const data2 = parseFile(filepath2);
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
 
-  console.log(data1);
-  console.log(data2);
-};
+  // Все ключи из обоих объектов
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
 
-export default genDiff;
+  const lines = keys.flatMap((key) => {
+    const hasKey1 = _.has(data1, key)
+    const hasKey2 = _.has(data2, key)
+
+    if (hasKey1 && hasKey2) {
+      if (data1[key] === data2[key]) {
+        return `    ${key}: ${data1[key]}`
+      }
+
+      return [
+        `  - ${key}: ${data1[key]}`,
+        `  + ${key}: ${data2[key]}`,
+      ]
+    }
+
+    if (hasKey1) {
+      return `  - ${key}: ${data1[key]}`
+    }
+
+    return `  + ${key}: ${data2[key]}`
+  })
+
+  return [
+    '{',
+    ...lines,
+    '}',
+  ].join('\n')
+}
+
+export default genDiff
