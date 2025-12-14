898c895dfec244c60f3f2fca5bce220a918c56e6
IT-Kitty <katerina.kvtn.skh@gmail.com>
2025-12-14 08:26:03 +0300

    style: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      ...stylistic.configs.recommended.rules,
+    },
+  },
+]
