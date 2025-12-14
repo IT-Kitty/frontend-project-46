648ac5b602f6e1cf9c7704217fdd57528d04fdc7
IT-Kitty <katerina.kvtn.skh@gmail.com>
2025-12-14 08:38:31 +0300

    test: create tests and add github actions

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import genDiff from '../index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const getFixturePath = filename =>
+  path.join(__dirname, '..', '__fixtures__', filename)
+
+test('flat json diff', () => {
+  const file1Path = getFixturePath('file1.json')
+  const file2Path = getFixturePath('file2.json')
+  const expected = fs.readFileSync(
+    getFixturePath('expected.txt'),
+    'utf-8',
+  )
+
+  const result = genDiff(file1Path, file2Path)
+
+  expect(result).toBe(expected)
+})
